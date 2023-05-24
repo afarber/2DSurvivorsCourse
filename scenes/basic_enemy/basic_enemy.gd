bd495705317f71b9ad3238fe6c83842d339801d2
Alexander Farber <farber72@outlook.de>
2023-05-24 23:14:46 +0200

    Spawn enemies just outside the screen

--- scenes/basic_enemy/basic_enemy.gd
+++ scenes/basic_enemy/basic_enemy.gd
@@ -1,20 +1,17 @@
 extends CharacterBody2D
 
-
 const MAX_SPEED = 75
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Area2D.area_entered.connect(on_area_entered)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	var direction = get_direction_to_player()
 	velocity = direction * MAX_SPEED
 	move_and_slide()
-	
+
 
 func get_direction_to_player():
 	var player_node = get_tree().get_first_node_in_group("player") as Node2D
