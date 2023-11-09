358ac4560961eff54f1b3effc753432ad2e221a8
Alexander Farber <farber72@outlook.de>
2023-11-09 18:47:00 +0100

    Add camera smoothing

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
+
 const MAX_SPEED = 200
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -12,6 +15,7 @@ func _process(delta):
 	var direction = movement_vector.normalized()
 	velocity = direction * MAX_SPEED
 	move_and_slide()
+
 
 func get_movement_vector():
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
