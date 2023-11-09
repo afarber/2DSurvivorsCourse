190b2219695df5b3a178829897dc0ef078498bcb
Alexander Farber <farber72@outlook.de>
2023-11-09 18:47:00 +0100

    Attack the closest enemy by sword

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -1,6 +1,9 @@
 extends Node
 
 
+const MAX_RANGE = 150
+
+
 @export var sword_ability:PackedScene
 
 
@@ -14,7 +17,20 @@ func on_timer_timeout():
 	if player_node == null:
 		return
 		
+	# find all enemies with MAX_RANGE distance to the player
+	var enemies = get_tree().get_nodes_in_group("enemy").filter(func(enemy: Node2D): 
+		return enemy.global_position.distance_squared_to(player_node.global_position) < MAX_RANGE * MAX_RANGE
+	)
+	if enemies.size() == 0:
+		return
+		
+	enemies.sort_custom(func(a: Node2D, b: Node2D):
+		var a_distance = a.global_position.distance_squared_to(player_node.global_position)
+		var b_distance = b.global_position.distance_squared_to(player_node.global_position)
+		return a_distance < b_distance
+	)
+		
 	var sword_instance = sword_ability.instantiate() as Node2D
 	player_node.get_parent().add_child(sword_instance)
-	sword_instance.global_position = player_node.global_position
+	sword_instance.global_position = enemies[0].global_position
 	
